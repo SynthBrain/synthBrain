0e1159ffed93b849674076c8a4498a30069924b1
Yevhenii Doroshenko <synth8rain@gmail.com>
2019-04-15 23:41:00 +0300

    ref

--- levelScene/lightsScene.go
+++ levelScene/lightsScene.go
@@ -8,9 +8,9 @@ import (
 
 // LightsScene add lights 
 func LightsScene(app *application.Application){
-	ambientLight := light.NewAmbient(&math32.Color{1.0, 1.0, 1.0}, 0.8)
+	ambientLight := light.NewAmbient(math32.NewColor("White"), 0.8)//{1.0, 1.0, 1.0}, 0.8)
 	app.Scene().Add(ambientLight)
-	pointLight := light.NewPoint(&math32.Color{1, 1, 1}, 5.0)
+	pointLight := light.NewPoint(math32.NewColor("White"), 5.0) //(&math32.Color{1, 1, 1}, 5.0)
 	pointLight.SetPosition(0, 0, 0)
 	app.Scene().Add(pointLight)
 }

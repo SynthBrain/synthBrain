e954edc828eb01d8ecfd4da95863a226950563b8
SynthBrain <synth8rain@gmail.com>
2019-05-05 17:19:50 +0300

    create method img to 2d slice

--- vision/startWebCam.go
+++ vision/startWebCam.go
@@ -60,37 +60,40 @@ func StartWebCam(chFlag chan bool) {
 	}
 }
 
-func massiveImg(img image.Image) [][]byte {
+func ImgToDataSlice(img image.Image) [][]byte {
 	bounds := img.Bounds()
-	//fmt.Println(bounds.Max.X, bounds.Max.Y)
-	data := make([][]byte, bounds.Size().Y) // create 1D slice size columns
+
+	data := make([][]byte, bounds.Size().X) // create 1D slice size columns
 	for i := bounds.Min.X; i < bounds.Max.X; i++ {
-		data[i] = make([]byte, bounds.Size().X) // create 2D slice size rows
+		data[i] = make([]byte, bounds.Size().Y) // create 2D slice size rows
 		for j := bounds.Min.Y; j < bounds.Max.Y; j++ {
 			r, g, b, _ := img.At(i, j).RGBA()
-			data[i][j] = byte((r + g + b) / 3)
+			//data[i][j] = byte((r + g + b) / 3)
+			data[i][j] = byte(((b >> 8) + (g >> 8) + (r >> 8)) / 3)
 		}
 	}
 	return data
 }
 
-//for i:= 0; i < len(data[i]); i++{
-//	for j:= 0; j < len(data[j]); j++{
-//		fmt.Print(data[i][j], " ")
+func Print2DSlice(data [][]byte) {
+	for i := 0; i < len(data[i]); i++ {
+		for j := 0; j < len(data[j]); j++ {
+			fmt.Print(data[i][j], " ")
+		}
+		fmt.Println()
+	}
+}
+
+//func myVision(img image.Image) []byte {
+//	bounds := img.Bounds()
+//	x := bounds.Dx()
+//	y := bounds.Dy()
+//	data := make([]byte, 0, x*y*3)
+//	for j := bounds.Min.Y; j < bounds.Max.Y; j++ {
+//		for i := bounds.Min.X; i < bounds.Max.X; i++ {
+//			r, g, b, _ := img.At(i, j).RGBA()
+//			data = append(data, byte(b>>8), byte(g>>8), byte(r>>8))
+//		}
 //	}
-//	fmt.Println()
+//	return data
 //}
-
-func myVision(img image.Image) []byte {
-	bounds := img.Bounds()
-	x := bounds.Dx()
-	y := bounds.Dy()
-	data := make([]byte, 0, x*y*3)
-	for j := bounds.Min.Y; j < bounds.Max.Y; j++ {
-		for i := bounds.Min.X; i < bounds.Max.X; i++ {
-			r, g, b, _ := img.At(i, j).RGBA()
-			data = append(data, byte(b>>8), byte(g>>8), byte(r>>8))
-		}
-	}
-	return data
-}
